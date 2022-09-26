d6c37abfc958129ec243d78d9acd2647ac4a01b3
kaazu1224 <75646380+kazun1224@users.noreply.github.com>
2022-09-27 00:47:46 +0900

    初期設定とmantineのインストール

---
+++ src/pages/_app.tsx
@@ -0,0 +1,30 @@
+import {
+  MantineProvider,
+  ColorSchemeProvider,
+  ColorScheme,
+} from "@mantine/core";
+import { AppProps } from "next/app";
+import { useState } from "react";
+import "src/styles/index.css";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  const [colorScheme, setColorScheme] = useState<ColorScheme>("light");
+  const toggleColorScheme = (value?: ColorScheme) =>
+    setColorScheme(value || (colorScheme === "dark" ? "light" : "dark"));
+  return (
+    <ColorSchemeProvider
+      colorScheme={colorScheme}
+      toggleColorScheme={toggleColorScheme}
+    >
+      <MantineProvider
+        theme={{ colorScheme }}
+        withGlobalStyles
+        withNormalizeCSS
+      >
+        <Component {...pageProps} />
+      </MantineProvider>
+    </ColorSchemeProvider>
+  );
+}
+
+export default MyApp;
