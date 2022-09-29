2731ee2435e498e4e5f57ca820743595067e9aa9
kaazu1224 <75646380+kazun1224@users.noreply.github.com>
2022-09-29 23:22:43 +0900

    共通のレイアウトを作成

---
+++ src/layouts/layout.tsx
@@ -0,0 +1,30 @@
+import { HeaderComponent } from "src/layouts/header";
+import { FooterComponent } from "src/layouts/footer";
+import { CustomLayout } from "next";
+import Head from "next/head";
+
+export const Layout: CustomLayout = (props) => {
+  const links = [
+    {
+      link: "/",
+      label: "Home",
+      links: [
+        {
+          link: "",
+          label: "",
+        },
+      ],
+    },
+  ];
+  return (
+    <>
+      <Head>
+        <title>Portfolio</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <HeaderComponent links={links} />
+      <main className="py-20 md:py-32">{props.children}</main>
+      <FooterComponent />
+    </>
+  );
+};
