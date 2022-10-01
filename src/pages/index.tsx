a13927326d622558248277443ccb8bde32084705
kaazu1224 <75646380+kazun1224@users.noreply.github.com>
2022-10-01 17:20:48 +0900

    不要なコンポーネントを削除

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,10 @@
 import Head from "next/head";
-import { CustomLayout, GetStaticProps,CustomNextPage } from "next";
+import { GetStaticProps,CustomNextPage } from "next";
 import Container from "src/components/container";
 import MoreStories from "src/components/more-stories";
 import HeroPost from "src/components/hero-post";
-import Intro from "src/components/intro";
-import Layout from "src/components/layout";
 import { Layout as LayoutCustom } from "src/layouts";
 import { getAllPostsForHome } from "src/lib/api";
-import { CMS_NAME } from "src/lib/constants";
-import { Button } from "@mantine/core";
 
 type Posts = {
   allPosts:any;
@@ -20,12 +16,11 @@ const Home: CustomNextPage<Posts> = ({ allPosts: { edges }, preview }) => {
   const morePosts = edges.slice(1);
 
   return (
-    <Layout preview={preview}>
+    <>
       <Head>
-        <title>Next.js Blog Example with {CMS_NAME}</title>
+        <title>Ocean Script</title>
       </Head>
       <Container>
-        <Intro />
         {heroPost && (
           <HeroPost
             title={heroPost.title}
@@ -38,7 +33,7 @@ const Home: CustomNextPage<Posts> = ({ allPosts: { edges }, preview }) => {
         )}
         {morePosts.length > 0 && <MoreStories posts={morePosts} />}
       </Container>
-    </Layout>
+    </>
   );
 };
 
