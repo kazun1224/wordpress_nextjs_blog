01cb0e8d80c27e6751d2b0c25dbd577bf1649560
kaazu1224 <75646380+kazun1224@users.noreply.github.com>
2022-10-02 16:22:01 +0900

    ✨

---
+++ src/components/Post/post-cards.tsx
@@ -0,0 +1,76 @@
+import { createStyles, Text, Card, SimpleGrid, Container } from "@mantine/core";
+import { Gauge, User, Cookie } from "tabler-icons-react";
+import { FC } from "react";
+
+const mockdata = [
+  {
+    title: "Extreme performance",
+    description:
+      "This dust is actually a powerful poison that will even make a pro wrestler sick, Regice cloaks itself with frigid air of -328 degrees Fahrenheit",
+    icon: Gauge,
+  },
+  {
+    title: "Privacy focused",
+    description:
+      "People say it can run at the same speed as lightning striking, Its icy body is so cold, it will not melt even if it is immersed in magma",
+    icon: User,
+  },
+  {
+    title: "No third parties",
+    description:
+      "They’re popular, but they’re rare. Trainers who show them off recklessly may be targeted by thieves",
+    icon: Cookie,
+  },
+];
+
+const useStyles = createStyles((theme) => ({
+  card: {
+    border: `1px solid ${
+      theme.colorScheme === "dark" ? theme.colors.dark[5] : theme.colors.gray[1]
+    }`,
+  },
+
+  cardTitle: {
+    "&::after": {
+      content: '""',
+      display: "block",
+      backgroundColor: theme.fn.primaryColor(),
+      width: 45,
+      height: 2,
+      marginTop: theme.spacing.sm,
+    },
+  },
+}));
+
+export const FeaturesCards: FC = () => {
+  const { classes, theme } = useStyles();
+  const features = mockdata.map((feature) => (
+    <Card
+      key={feature.title}
+      shadow="md"
+      radius="md"
+      className={classes.card}
+      p="xl"
+    >
+      <feature.icon size={50} strokeWidth={2} color={theme.fn.primaryColor()} />
+      <Text size="lg" weight={500} className={classes.cardTitle} mt="md">
+        {feature.title}
+      </Text>
+      <Text size="sm" color="dimmed" mt="sm">
+        {feature.description}
+      </Text>
+    </Card>
+  ));
+  return (
+    <Container size="lg" py="xl">
+      <SimpleGrid
+        cols={3}
+        spacing="xl"
+        mt={50}
+        breakpoints={[{ maxWidth: "md", cols: 1 }]}
+      >
+        {features}
+      </SimpleGrid>
+    </Container>
+  );
+};
---
+++ src/pages/articles.tsx
@@ -0,0 +1,51 @@
+import Head from "next/head";
+import { GetStaticProps,CustomNextPage, GetServerSideProps } from "next";
+import { Container } from '@mantine/core';
+import MoreStories from "src/components/more-stories";
+import HeroPost from "src/components/hero-post";
+import { Layout as LayoutCustom } from "src/layouts";
+import { getAllPostsForHome } from "src/lib/api";
+
+type Posts = {
+  allPosts:any;
+  preview: any;
+}
+
+const Articles: CustomNextPage<Posts> = ({ allPosts: { edges }, preview }) => {
+  const heroPost = edges[0]?.node;
+  const morePosts = edges.slice(1);
+
+  return (
+    <>
+      <Head>
+        <title>Ocean Script</title>
+      </Head>
+      <Container>
+        {heroPost && (
+          <HeroPost
+            title={heroPost.title}
+            coverImage={heroPost.featuredImage}
+            date={heroPost.date}
+            author={heroPost.author}
+            slug={heroPost.slug}
+            excerpt={heroPost.excerpt}
+          />
+        )}
+        {morePosts.length > 0 && <MoreStories posts={morePosts} />}
+      </Container>
+    </>
+  );
+};
+
+export const getStaticProps: GetStaticProps  = async ({ preview = false }) => {
+  const allPosts = await getAllPostsForHome(preview);
+
+  return {
+    props: { allPosts, preview },
+    revalidate: 10,
+  };
+};
+
+Articles.getLayout = (page) => <LayoutCustom>{page}</LayoutCustom>;
+
+export default Articles;
